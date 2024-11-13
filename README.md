be13e064aeaf070858b2f9738a10e088781d6702
Raul <80535950+hideNFN@users.noreply.github.com>
2024-11-13 14:49:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# hideN's MP3 Downloader
+### This is a simple terminal MP3 Downloader solution that is based on the `yt-dlp` repository and has a freeze.py file with `py2exe` build instructions. The program creates all the needed files but you still need to download FFmpeg for everything to work.
+
+### Usage
+This tool works with a multitude amount of websites, you just have to place the link of the video/audio file and it downloads it. The script uses mp3 by default for compatibility purposes but it can be easily switched by changing the `"preferredcodec": "mp3"` line and putting whatever codec you would like in place of "mp3".
+Playlists are detected automatically on websites such as YouTube, Bandcamp, etc. and the program will download the audio files one after another.
+### Build
+
+Use the `python freeze.py` command after you cd into the `master` folder.
+
+### Sources:
+
+[yt-dlp](https://github.com/yt-dlp/yt-dlp)
+
+[FFmpeg website](https://ffmpeg.org/)
+
+[py2exe](https://github.com/py2exe/py2exe)
---
+++ master/mp3downloader.py
@@ -0,0 +1,48 @@
+import yt_dlp
+import os
+
+print("\nMP3 Downloader v1.1.0 by hideNFN\n")
+
+currentfolder = os.getcwd()
+datafolder = currentfolder + "\\.data"
+ffmpegfolder = datafolder + "\\.ffmpeg"
+ffmpegcheck = ffmpegfolder + "\\ffmpeg.exe"
+ffprobecheck = ffmpegfolder + "\\ffprobe.exe"
+
+
+if os.path.exists(datafolder) is False or os.path.exists(ffmpegfolder) is False:
+    print("\nCreating necessary folders...")
+    if os.path.exists(datafolder) is False:
+        os.mkdir(datafolder)
+    if os.path.exists(ffmpegfolder) is False:
+        os.mkdir(ffmpegfolder)
+
+if os.path.exists(ffmpegcheck) is True and os.path.exists(ffprobecheck) is True:
+    print("\nThe FFmpeg install has been found.\n")
+else:
+    print('\nThe FFmpeg install hasn\'t been found, please download FFmpeg and place ffmpeg.exe and ffprobe.exe inside the "\.data\.ffmpeg" path.\n')
+
+os.chdir(datafolder)
+
+def mp3dwn():
+    while True:
+        print("\nPaste the URL of the media you would like to download in a mp3 format:\n")
+
+        linkmp3 = input()
+
+        try:
+            ydlp_opts = {
+                "ffmpeg_location": ".ffmpeg",
+                "outtmpl": "%(title)s.%(ext)s",
+                "format": "bestaudio/best",
+                "postprocessors": [{
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }],
+            }
+            with yt_dlp.YoutubeDL(ydlp_opts) as ydlp:
+                ydlp.download([linkmp3])
+        except:
+            print("Processing error.")
+mp3dwn()
